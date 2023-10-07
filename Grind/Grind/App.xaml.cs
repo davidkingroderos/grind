1a6a87cc6ede691cc474ecf57ce684b85edcb77c
David King Roderos <iamdavidkingroderos@gmail.com>
2023-10-07 19:05:02 +0800

    Grind Initial Commit

---
+++ Grind/Grind/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Grind
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Grind/Grind/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Grind
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
