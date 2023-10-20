590aae457b9a8ff9b6a56e0788f83ec19ce901af
David King Roderos <iamdavidkingroderos@gmail.com>
2023-10-20 23:27:13 +0800

    Added other pages

---
+++ src/Grind/View/RoutinePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Grind.View;
+
+public partial class RoutinePage : ContentPage
+{
+	public RoutinePage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/Grind/View/ToolsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Grind.View;
+
+public partial class ToolsPage : ContentPage
+{
+	public ToolsPage()
+	{
+		InitializeComponent();
+	}
+}
