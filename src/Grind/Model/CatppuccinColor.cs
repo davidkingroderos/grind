6510255451da85ddc3ae71a9bb6c5fd7ef77690e
David King Roderos <iamdavidkingroderos@gmail.com>
2023-10-23 23:34:15 +0800

    Added color enum

---
+++ src/Grind/Model/CatppuccinColor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grind.Model
+{
+    public enum CatppuccinColor
+    {
+        Rosewater = 1,
+        Flamingo = 2,
+        Pink = 3,
+        Mauve = 4,
+        Red = 5,
+        Maroon = 6,
+        Peach = 7,
+        Yellow = 8,
+        Green = 9,
+        Teal = 10,
+        Sky = 11,
+        Sapphire = 12,
+        Blue = 13,
+        Lavender = 14,
+    }
+}
