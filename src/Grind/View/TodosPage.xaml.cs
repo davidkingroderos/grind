02ca9816d640d8a47ffc7f600f7adcc55b8f2d97
David King Roderos <iamdavidkingroderos@gmail.com>
2023-11-01 00:47:29 +0800

    Implemented auto refreshing on Todos

--- src/Grind/View/TodosPage.xaml.cs
+++ src/Grind/View/TodosPage.xaml.cs
@@ -10,4 +10,16 @@ public partial class TodosPage : ContentPage
 
 		BindingContext = viewModel;
 	}
+
+    protected override void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        base.OnNavigatedTo(args);
+
+		var viewModel = BindingContext as TodosViewModel;
+
+		if (viewModel.GetTodosCommand.CanExecute(null))
+		{
+			viewModel.GetTodosCommand.Execute(null);
+		}
+    }
 }
