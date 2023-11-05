6dc0f7aa7e7bfec7c3f3e3e9f549ad75f7229973
David King Roderos <iamdavidkingroderos@gmail.com>
2023-11-06 03:36:28 +0800

    Added todo detail properties

--- src/Grind/ViewModels/Todos/TodoDetailsViewModel.cs
+++ src/Grind/ViewModels/Todos/TodoDetailsViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Grind.Models;
+using Grind.Services;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +16,43 @@ namespace Grind.ViewsModels
     {
         public TodoDetailsViewModel()
         {
+            Application.Current.RequestedThemeChanged += (s, a) =>
+            {
+                Todo.ThemeColor = CatppuccinColorConverter.GetColor(Todo.Color);
+            };
         }
 
         [ObservableProperty]
         private Todo todo;
+
+        [ObservableProperty]
+        private string color;
+
+        [ObservableProperty]
+        public string name;
+
+        [ObservableProperty]
+        public string description;
+
+        [RelayCommand]
+        private async Task UpdateTodoAsync()
+        {
+            try
+            {
+
+
+
+                await Shell.Current.CurrentPage.DisplayAlert("Success!",
+                    $"Todo updated", "OK");
+
+                await Shell.Current.GoToAsync("..");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                await Shell.Current.CurrentPage.DisplayAlert("Error!",
+                    $"Unable to add Todo: {ex.Message}", "OK");
+            }
+        }
     }
 }
